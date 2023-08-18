4f4fa7b642f7ae3b4e9d2194313069a7e8749637
RitikRajvanshi <103512965+RitikRajvanshi@users.noreply.github.com>
2023-08-18 13:36:52 +0530

    Src/multiple folder

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  //hostUrl:'http://31.220.56.32:2000/',
+  hostUrl:'http://localhost:2000/',
+  //hostUrl:'http://localhost:2000/',
+  //hostUrl:'http://192.168.0.111:2000/',
+  production: true
+};
